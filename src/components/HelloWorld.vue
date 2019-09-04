49ce8ee93f820eb2838fe776600f7342d50f71c6
Tore Dahl <tore.dahl@bouvet.no>
2019-09-04 16:13:06 +0200

    first commit: testing changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2> THIS IS HELIKS ON A ROLL </h2>
+    ------------ o||o ------------
+                  /\
+                ||||||
+                ~~~~~~
+
     <h2>Essential Links</h2>
     <ul>
       <li>
